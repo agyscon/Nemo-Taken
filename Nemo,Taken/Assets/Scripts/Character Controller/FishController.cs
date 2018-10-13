43e6e7f898dd1c4538e7c8d357c58e78fdbd233e
agyscon 67 <agysconner67@gmail.com>
2018-10-13 00:04:42 -0400

    experiment with branches

---
+++ Nemo,Taken/Assets/Scripts/Character Controller/FishController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishController : MonoBehaviour {
+    public float movementSpeed = 5;
+
+	
+	// Update is called once per frame
+	void Update () {
+        float translateX = Input.GetAxis("Horizontal") * 5 * Time.deltaTime;
+        float translateZ = Input.GetAxis("Vertical") * 5 * Time.deltaTime;
+        transform.Translate(translateX, 0, translateZ);
+	}
+}
